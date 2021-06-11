84342846b60387fc5f54f2136cc89744ba277802
K-Tatsuya514 <61820631+K-Tatsuya514@users.noreply.github.com>
2021-06-11 11:17:13 +0900

    [finish]

---
+++ js/pan.js
@@ -0,0 +1,58 @@
+<script type="application/ld+json">
+[
+{
+  "@context": "https://schema.org",
+  "@type": "BreadcrumbList",
+  "itemListElement": [
+    {
+      "@type": "ListItem",
+      "position": 1,
+      "name": "トップ",
+      "item": "http://ckoch.html.xdomain.jp/"
+    },
+    {
+      "@type": "ListItem",
+      "position": 2,
+      "name": "お部屋",
+      "item": "http://ckoch.html.xdomain.jp/room.html"
+    }
+  ]
+},
+{
+  "@context": "https://schema.org",
+  "@type": "BreadcrumbList",
+  "itemListElement": [
+    {
+      "@type": "ListItem",
+      "position": 1,
+      "name": "トップ",
+      "item": "http://ckoch.html.xdomain.jp/"
+    },
+    {
+      "@type": "ListItem",
+      "position": 2,
+      "name": "お料理",
+      "item": "http://ckoch.html.xdomain.jp/cuisine.html"
+    }
+  ]
+},
+{
+  "@context": "https://schema.org",
+  "@type": "BreadcrumbList",
+  "itemListElement": [
+    {
+      "@type": "ListItem",
+      "position": 1,
+      "name": "トップ",
+      "item": "http://ckoch.html.xdomain.jp/"
+    },
+    {
+      "@type": "ListItem",
+      "position": 2,
+      "name": "温泉",
+      "item": "http://ckoch.html.xdomain.jp/onsen.html"
+    }
+  ]
+}
+]
+</script>
